293967b4ef307cbf338e900fdee25f4d5eeeed79
SantiagoCorrea1 <santiago.correac1@udea.edu.co>
2026-05-18 15:10:26 -0500

    Refactor evaluar_regresion_robusta function parameters and remove unused import

--- myanswers/answer-639.py
+++ myanswers/answer-639.py
@@ -1,19 +1,18 @@
 import pandas as pd
 import numpy as np
-import random
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import TheilSenRegressor
 from sklearn.metrics import median_absolute_error
 
-def evaluar_regresion_robusta(df, columnas, columna_objetivo, test_size):
+def evaluar_regresion_robusta(df, columnas, target_col, test_size):
     X = df[columnas].to_numpy()
-    y = df[columna_objetivo].to_numpy()
+    y = df[target_col].to_numpy()
     
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=test_size, random_state=42
     )
     
-    model = TheilSenRegressor(random_state=42)
+    model = TheilSenRegressor()
     model.fit(X_train, y_train)
     
     y_pred = model.predict(X_test)
