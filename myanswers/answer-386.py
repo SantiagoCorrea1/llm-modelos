462aacf277ac1a92015014b93e0ce89d565b237f
SantiagoCorrea1 <santiago.correac1@udea.edu.co>
2026-05-18 15:12:27 -0500

    Fix parameter name in evaluar_regresion_robusta function for consistency

--- myanswers/answer-386.py
+++ myanswers/answer-386.py
@@ -4,9 +4,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import TheilSenRegressor
 from sklearn.metrics import median_absolute_error
 
-def evaluar_regresion_robusta(df, columnas, target_col, test_size):
+def evaluar_regresion_robusta(df, columnas, columna_objetivo, test_size):
     X = df[columnas].to_numpy()
-    y = df[target_col].to_numpy()
+    y = df[columna_objetivo].to_numpy()
     
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=test_size, random_state=42
